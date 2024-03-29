64c987c6811ce86856a83b57c2e511fb4efafad4
Harmony <harmony@example.com>
2024-03-29 02:22:13 -0500

    sign-in feature complete

--- public/app.js
+++ public/app.js
@@ -1,14 +1,9 @@
+auth.signOut();
 function r_e(id) {
   return document.querySelector(`#${id}`);
 }
 
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-        
-//         //FIREBASE CODE
-// let auth = firebase.auth();
-// let db = firebase.firestore();
+var login_status = 0;
 
 //Home Page
 //Login
@@ -25,58 +20,40 @@ create_acct = r_e("create_acct");
 const closeModal = document.getElementById("close-modal");
 
 login.addEventListener("click", () => {
-  r_e("signup_form").reset();
-  modalHome.classList.add("is-active");
-});
+  console.log("login button clicked");
+    if (login_status==1) {
+      // User is signed in.
+      login.innerHTML = "Login";
+      console.log('In if statement');
+      login_status=0;
+    } else {
+      // No user is signed in.
+      r_e("signup_form").reset();
+      modalHome.classList.add("is-active");
+      console.log('In else statement');
+    }
+  });
 
 modalBG.addEventListener("click", () => {
   modalHome.classList.remove("is-active");
 });
 
-// r_e('signUp_btn').addEventListener('click', () => {
-//   let email = r_e('user_name').value;
-//   let password = r_e('main_pass').value;
-//   console.log(email,password);
-//   log_in(email, password);
-//   });
+r_e('signUp_btn').addEventListener('click', () => {
+    let email = r_e('user_name').value;
+    let password = r_e('main_pass').value;
+    console.log("signup button clicked");
+      auth.signInWithEmailAndPassword(email, password).then((user) => {
+        console.log("login_success");
+        login_status=1;
 
-  // function log_in(email, password){
-  //   auth.signInWithEmailAndPassword(email, password).then((user) => {
-  //     login.reset();
-  //     modalHome.classList.remove("is-active");
-  //     user_in = auth.currentUser.email;
+    // reset the form
+    r_e("signup_form").reset();
 
-  //   }).catch((error) =>{
-  //     alert("invalid input")
-  //     r_e('login').reset();
-  //   });
-  //   };
+    // hide the modal
+    modalHome.classList.remove("is-active");
 
-// create_acct.addEventListener('click', () => {
-//   modalHome.classList.remove('is-active')
-//   modalnewAcct.classList.add('is-active')
-// });
+    //change login button to sign out
+    login.innerHTML = "Sign Out";
+  });
+});
 
-// modalBG_new.addEventListener('click', () =>{
-//   modalnewAcct.classList.remove('is-active')
-// });
-
-//import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-// sign_in = r_c('signUp_btn');
-
-// sign_in.addEventListener('click', () => {
-
-// });
-
-// const auth = getAuth();
-// signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//   });
