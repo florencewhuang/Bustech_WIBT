fa965fbaf42d207a5ab08314d041d744c8268b4a
Harmony <harmony@example.com>
2024-03-29 02:52:35 -0500

    started the addevents js, collecting info

--- public/app.js
+++ public/app.js
@@ -57,3 +57,14 @@ r_e('signUp_btn').addEventListener('click', () => {
   });
 });
 
+///////////////////////////////////////////////////////////
+//Add Event Page
+r_e("add_enter").addEventListener('click', () =>{
+  console.log("enter clicked");
+  let event_name = r_e('add_name').value;
+  let event_type = r_e('add_dropdown').value;
+  let event_date = r_e('add_date').value;
+  let event_description = r_e('add_description').value;
+  console.log(event_name,event_type, event_date, event_description);
+});
+
