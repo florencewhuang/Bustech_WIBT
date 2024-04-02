afe05dd704c9776d86e9238fb82925dbbebc4a1f
rashmi <rashmi@example.com>
2024-04-02 17:14:26 -0500

    fixed error with page and finished js

--- public/eventlib.js
+++ public/eventlib.js
@@ -1,18 +1,18 @@
-// function r_e(id) {
-//   return document.querySelector(`#${id}`);
-// }
+function r_e(id) {
+  return document.querySelector(`#${id}`);
+}
 
-// r_e("submit_search").addEventListener("click", () => {
-//   console.log("event search submitted");
-//   //   let keyword_search = r_e("keyword_search").value;
-//   //   let event_type_search = r_e("typesearch_dropdown").value;
-//   //   let date_search = r_e("search_date").value;
+r_e("submit_search").addEventListener("click", () => {
+  console.log("event search submitted");
+  let keyword_search = r_e("keyword_search").value;
+  let event_type_search = r_e("typesearch_dropdown").value;
+  let date_search = r_e("search_date").value;
 
-//   //   let searched_event = {
-//   //     keyword: keyword_search,
-//   //     type: event_type_search,
-//   //     date: date_search,
-//   //   };
+  let searched_event = {
+    keyword: keyword_search,
+    type: event_type_search,
+    date: date_search,
+  };
 
-//   //   console.log(searched_event);
-// });
+  console.log(searched_event);
+});
