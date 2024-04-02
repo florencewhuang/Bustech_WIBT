46d39366d261219e513a94697e88e0d06ea385c9
Harmony <harmony@example.com>
2024-04-02 16:58:48 -0500

    pulling data from database

--- public/schedule.js
+++ public/schedule.js
@@ -3,67 +3,11 @@ function r_e(id) {
   };
 
   r_e('testing').addEventListener('click', () =>{
-    print_data('IBM Speaker', '03/03/2024', 'This event will take place on Zoom at 5pm featuring Kristyn Arends from IBM. Please come prepared with questions to ask her!')
-
+    show_events()
   });
 
 
-
-  function print_data(title,date,desc){
-    r_e('event_area').innerHTML =         
-        `<div class="schedule-card">
-        <div class="schedule-card-container">
-          <div class="card-header">
-            <h2 class="results-headers">${title}</h2>
-          </div>
-          <br />
-          <h3 class="schedule-date" style="font-style: italic">
-            Event Date: ${date}
-          </h3>
-          <br />
-          <img
-             src="images/Speaker_Event_icon.png"
-            alt=""
-            width="25%"
-            height="40%"
-            style="padding-top: 10px; padding-bottom: 10px"
-          />
-          <p>
-          ${desc}
-          </p>
-        </div>
-      </div>`
-  };
-
-
-  db.collection("event library").get().then((data) => {
-    let mydocs = data.docs;
-
-    let html = ``;
-    mydocs.forEach((doc) => {
-
-      let title = doc.data().name;
-
-      html += `
-
-      <div class="box" id="${doc.id}">
-
-          <h1 class="subtitle has-text-weight-bold"> ${doc.data().title} 
-            <span class="is-pulled-right is-clickable has-background-danger p-3" onclick = "deleteDoc('${
-              doc.id
-            }')" >X</span>
-          
-          </h1>
-          <p>Added by: ${doc.data().added_by}</p>
-          <p>${doc.data().desc}</p>        
-      </div>
-      
-      `;
-    });
-    content.innerHTML = html;
-  });
-
-  function show_events(title, date, img, desc){
+  function show_events(){
     db.collection("event library")
     .get()
     .then((data) => {
@@ -71,32 +15,32 @@ function r_e(id) {
 
       let html = ``;
       mydocs.forEach((doc) => {
-        html += `       
+        html += `
         <div class="schedule-card">
         <div class="schedule-card-container">
           <div class="card-header">
-            <h2 class="results-headers">${title}</h2>
+            <h2 class="results-headers">${doc.data().name}</h2>
           </div>
           <br />
           <h3 class="schedule-date" style="font-style: italic">
-            Event Date: ${date}
+            Event Date:${doc.data().date}
           </h3>
           <br />
           <img
-             src=${img}
+             src= ${doc.data().image_id}
             alt=""
             width="25%"
             height="40%"
             style="padding-top: 10px; padding-bottom: 10px"
           />
           <p>
-          ${desc}
+          ${doc.data().description}
           </p>
         </div>
       </div>
-        
         `;
       });
-      content.innerHTML = html;
+
+      r_e('event_area').innerHTML = html;
     });
-  }
+  };
