f39d97896d0c1468cda968b77856804a371c64aa
abby <vitou@wisc.edu>
2024-04-04 17:40:03 -0500

    all javascript loaded onto the page

--- public/app.js
+++ public/app.js
@@ -338,22 +338,52 @@ var login_status = 0;
 
 login = r_e("login");
 
-modalBG = r_e("modalBG");
-
 modalHome = r_e("modalHome");
-
-modalnewAcct = r_e("modalnewAcct");
-
-create_acct = r_e("create_acct");
 
 const closeModal = document.getElementById("close-modal");
 
 login.addEventListener("click", () => {
-  modalHome.classList.add("is-active");
+  if (login_status == 1) {
+    // User is signed in.
+    login.innerHTML = "Login";
+    console.log("In if statement");
+    login_status = 0;
+  } else {
+    // No user is signed in.
+    r_e("login_form").reset();
+    modalHome.classList.add("is-active");
+  }
 });
 
 closeModal.addEventListener("click", () => {
   modalHome.classList.remove("is-active");
+});
+
+r_e("login_reset").addEventListener("click", () => {
+  r_e("login_form").reset();
+});
+
+r_e("signUp_btn").addEventListener("click", () => {
+  let email = r_e("user_name").value;
+  let password = r_e("main_pass").value;
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      login_status = 1;
+
+      // reset the form
+      r_e("login_form").reset();
+
+      // hide the modal
+      modalHome.classList.remove("is-active");
+
+      //change login button to sign out
+      login.innerHTML = "Sign Out";
+    })
+    .catch((error) => {
+      alert("Incorrect email or password");
+      r_e("login_form").reset();
+    });
 });
 
 // Get Todays Date Function
