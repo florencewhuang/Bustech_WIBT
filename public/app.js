15c17072125a73ff1149ac758b09f23f9cabd6d3
Florence <fwhuang@wisc.edu>
2024-04-15 12:28:59 -0500

    DEI image

--- public/app.js
+++ public/app.js
@@ -568,7 +568,7 @@ r_e("addeventspage").addEventListener("click", () => {
       return "images/professional_development_icon.png";
     }
     if (event_type == "DEI") {
-      return "images/DEI.png";
+      return "images/DEI_image.png";
     }
     if (event_type == "Fundraiser") {
       return "images/fundraiser_icon.png";
